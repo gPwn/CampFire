c88d5ce4cfd56edf947d6cf3b62f9fe1d3e053a0
CHUCHUDACHUCHU <yoon7548@gmail.com>
2023-01-02 14:56:54 +0900

    <FEAT> 호스트용 로그인/회원가입 기능 구현 :sparkles:

--- src/routes/hosts.route.js
+++ src/routes/hosts.route.js
@@ -3,10 +3,10 @@ const HostsController = require('../architecture/controllers/hosts.controller');
 const hostsController = new HostsController();
 const router = express.Router();
 const upload = require('../modules/profileImg.js');
-const authLoginMiddleware = require('../middlewares/authLogin.middleware');
+const authLoginHostMiddleware = require('../middlewares/authLoginHost.middleware');
 
 router.post('/signup', upload.single('profileImg'), hostsController.signUp);
 router.get('/signup/findDup', hostsController.findDup);
-router.post('/login', authLoginMiddleware, hostsController.logIn);
+router.post('/login', authLoginHostMiddleware, hostsController.logIn);
 
 module.exports = router;

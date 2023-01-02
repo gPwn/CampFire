c0c8bf959c2c80e762750f07644ae3634577c844
CHUCHUDACHUCHU <yoon7548@gmail.com>
2023-01-02 10:15:18 +0900

    <CHORE> update app.js :art:

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/camps', require('./camps.route.js'));
+//router.use('/camps', require('./camps.route.js'));
 
 router.get('/', (req, res) => {
     res.send('api routes 연결');

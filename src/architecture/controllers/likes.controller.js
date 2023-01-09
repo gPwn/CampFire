6c9777078aa164d48f5cfd6b94ce4362eb97601f
gPwn <hyeju3014@gmail.com>
2023-01-09 23:48:57 +0900

    #116 <FEAT> 캠핑장 찜하기,찜 취소하기 기능 구현 :sparkles:

--- src/architecture/controllers/likes.controller.js
+++ src/architecture/controllers/likes.controller.js
@@ -7,15 +7,30 @@ class LikesController {
 
     addLike = async (req, res, next) => {
         try {
-            console.log('라이크 컨트롤러');
             const { campId } = req.params;
-            // const { userId } = res.locals;
-            const userId = 2;
+            const { userId } = res.locals;
 
             await this.likesService.addLike(campId, userId);
 
             res.status(201).json({
                 message: '캠핑장 찜하기를 성공하였습니다.',
+                campId,
+                userId,
+            });
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    deleteLike = async (req, res, next) => {
+        try {
+            const { campId } = req.params;
+            const { userId } = res.locals;
+
+            await this.likesService.deleteLike(campId, userId);
+
+            res.status(201).json({
+                message: '캠핑장 찜하기를 취소하였습니다.',
                 campId,
                 userId,
             });
--- src/architecture/repositories/likes.repository.js
+++ src/architecture/repositories/likes.repository.js
@@ -1,5 +1,32 @@
+const { Likes, Users, Camps } = require('../../models');
+
 class LikesRepository {
     constructor() {}
+
+    addLike = async (campId, userId) => {
+        return await Likes.create({
+            campId,
+            userId,
+        });
+    };
+
+    userLikeupdate = async (userId, userlike) => {
+        await Users.update(
+            {
+                likes: userlike,
+            },
+            { where: { userId } }
+        );
+    };
+
+    campLikeupdate = async (campId, camplike) => {
+        await Camps.update(
+            {
+                likes: camplike,
+            },
+            { where: { campId } }
+        );
+    };
 }
 
 module.exports = LikesRepository;

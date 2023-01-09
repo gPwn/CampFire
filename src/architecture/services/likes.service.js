c11ea50f4e02f8e7ca1ca4ea8222dc9bcc8101c4
gPwn <hyeju3014@gmail.com>
2023-01-09 23:46:31 +0900

    <FEAT> 캠핑장 찜하기,찜 취소하기 기능 구현 :sparkle:

--- src/architecture/services/likes.service.js
+++ src/architecture/services/likes.service.js
@@ -1,7 +1,11 @@
 const LikesRepository = require('../repositories/likes.repository.js');
 const UsersRepository = require('../repositories/users.repository.js');
 const CampsRepository = require('../repositories/camps.repository.js');
-
+const {
+    ValidationError,
+    InvalidParamsError,
+    ExistError,
+} = require('../../middlewares/exceptions/error.class.js');
 const {
     Books,
     Camps,
@@ -16,19 +20,40 @@ const {
 } = require('../../models');
 
 class LikesService {
+    constructor() {
+        this.liksRepository = new LikesRepository();
+    }
     usersRepository = new UsersRepository(Users);
     campsRepository = new CampsRepository(Books, Camps, Hosts, Users);
-    liksRepository = new UsersRepository(Users);
 
     addLike = async (campId, userId) => {
-        console.log(campId, userId);
         const findUser = await this.usersRepository.findOneUser(userId);
         const findCamps = await this.campsRepository.findCampById(campId);
 
-        console.log(findUser);
-        console.log(findCamps);
+        if (!findCamps) {
+            throw new InvalidParamsError('찜할 수 없는 캠핑장입니다.', 400);
+        }
 
-        await this.LikesRepository;
+        const userlike = Number(findUser.likes) + Number(1);
+        const camplike = Number(findCamps.likes) + Number(1);
+
+        await this.liksRepository.userLikeupdate(userId, userlike);
+        await this.liksRepository.campLikeupdate(campId, camplike);
+
+        return await this.liksRepository.addLike(campId, userId);
+    };
+
+    deleteLike = async (campId, userId) => {
+        const findUser = await this.usersRepository.findOneUser(userId);
+        const findCamps = await this.campsRepository.findCampById(campId);
+
+        const userlike = Number(findUser.likes) - Number(1);
+        const camplike = Number(findCamps.likes) - Number(1);
+
+        await this.liksRepository.userLikeupdate(userId, userlike);
+        await this.liksRepository.campLikeupdate(campId, camplike);
+
+        return await this.liksRepository.addLike(campId, userId);
     };
 }
 
--- src/routes/likes.route.js
+++ src/routes/likes.route.js
@@ -6,6 +6,8 @@ const LikesController = require('../architecture/controllers/likes.controller.js
 const likesController = new LikesController();
 
 // 유저 찜하기 기능
-router.put('/:campId', /*authUserMiddleware,*/ likesController.addLike);
+router.post('/:campId', authUserMiddleware, likesController.addLike);
+// 유저 찜하기 취소 기능
+router.delete('/:campId', authUserMiddleware, likesController.deleteLike);
 
 module.exports = router;

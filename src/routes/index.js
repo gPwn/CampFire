33a6c224b8692bbf17f381fa5bc6496ba96ca2d2
gPwn <hyeju3014@gmail.com>
2023-01-18 10:47:10 +0900

    <FIX> cicd test :art:

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ router.use('/camps', require('./reviews.route.js'));
 router.use('/search', require('./search.route.js'));
 
 router.get('/', (req, res) => {
-    res.send('api routes 연결 cicd test!!0112');
+    res.send('api routes 연결 cicd test!!0118');
 });
 
 module.exports = router;

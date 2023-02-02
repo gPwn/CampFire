08d81671f9422be2283552155544d3dc9f533927
gPwn <hyeju3014@gmail.com>
2023-02-02 21:38:17 +0900

    <FEAT> 이벤트 관련 조회 기능 구현 추가 :sparckles:

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -10,6 +10,7 @@ router.use('/camps', require('./sites.route.js'));
 router.use('/likes', require('./likes.route.js'));
 router.use('/camps', require('./reviews.route.js'));
 router.use('/search', require('./search.route.js'));
+router.use('/events', require('./events.route.js'));
 
 router.get('/', (req, res) => {
     res.send('api index route 연결 성공!!!');
